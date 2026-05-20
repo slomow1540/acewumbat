294cdcebe436b2678aa9c0b5a642d0b904d248c6
Slomow1540 <sanminvis555@gmail.com>
2026-05-20 17:54:08 +0700

    tempet simpan value "global"

---
+++ Assets/Script/Util/ValueHolder.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ValueHolder : MonoBehaviour
+{
+    public static ValueHolder Instance;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

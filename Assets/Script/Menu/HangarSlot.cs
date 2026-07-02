2fddd231c4234d760ed559c9be366409277ef2d3
EmpthyLL <javileh40@gmail.com>
2026-07-02 23:35:39 +0700

    fix merge

--- Assets/Script/Menu/HangarSlot.cs
+++ Assets/Script/Menu/HangarSlot.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using Util;
 
 public class HangarSlot : MonoBehaviour
 {
--- Assets/Script/Menu/SlotManager.cs
+++ Assets/Script/Menu/SlotManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using Util;
 
 public class SlotManager : MonoBehaviour
 {
